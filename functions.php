8cfa4ff550d03623730252685be92cad24d5a983
Thibault <th.jullion@gmail.com>
2017-04-04 15:20:15 +0200

    Valeurs hashtag

--- functions.php
+++ functions.php
@@ -2,17 +2,17 @@
 
 // array(NOM_CANDIDAT,TWITTER,MOTS_CLEFS)
 array(
-    array("Nathalie Arthaud","n_arthaud",array("NathalieArthaud","")),
-    array("Jean Luc Melenchon","JLMelenchon",array("JeanLucMelenchon","")),
-    array("Benoît Hamon","benoithamon",array("BenoîtHamon","")),
-    array("Nicolas Dupont Aignan","dupontaignan",array("NicolasDupontAignan","")),
-    array("Jean Lassalle","jeanlassalle",array("JeanLassalle","")),
-    array("Emmanuel Macron","EmmanuelMacron",array("EmmanuelMacron","")),
-    array("Marine Le Pen","MLP_officiel",array("MarineLePen","")),
-    array("Philippe Poutou","PhilippePoutou",array("PhilippePoutou","")),
-    array("Jacques Cheminad","Chaminade_HS",array("JacquesCheminad","")),
-    array("François Asselineau","UPR_Asselineau",array("FrançoisAsselineau","")),
-    array("François Fillon","FrancoisFillon",array("FrançoisFillon",""))
+    array("Nathalie Arthaud","n_arthaud",array("NathalieArthaud","arthaud")),
+    array("Jean Luc Melenchon","JLMelenchon",array("JeanLucMelenchon","JLM")),
+    array("Benoît Hamon","benoithamon",array("BenoitHamon","hamon")),
+    array("Nicolas Dupont Aignan","dupontaignan",array("NicolasDupontAignan","duponaignan")),
+    array("Jean Lassalle","jeanlassalle",array("JeanLassalle","lasalle")),
+    array("Emmanuel Macron","EmmanuelMacron",array("EmmanuelMacron","macron")),
+    array("Marine Le Pen","MLP_officiel",array("MarineLePen","MLP")),
+    array("Philippe Poutou","PhilippePoutou",array("PhilippePoutou","poutou")),
+    array("Jacques Cheminad","Chaminade_HS",array("JacquesCheminad","cheminad")),
+    array("François Asselineau","UPR_Asselineau",array("FrançoisAsselineau","asselineau")),
+    array("François Fillon","FrancoisFillon",array("FrançoisFillon","fillon"))
 );
 
 
