9fad44f5ba2233dd0c5cb9c28e0aa87442891d15
Thibault <th.jullion@gmail.com>
2017-04-04 15:08:08 +0200

    Comptes twitter

--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php
 
+array(
+    "NathalieArthaud" => "n_arthaud",
+    "JeanLucMelenchon" => "JLMelenchon",
+    "BenoîtHamon" => "benoithamon",
+    "NicolasDupontAignan" => "dupontaignan",
+    "JeanLassalle" => "jeanlassalle",
+    "EmmanuelMacron" => "EmmanuelMacron",
+    "MarineLePen" => "MLP_officiel",
+    "PhilippePoutou" => "PhilippePoutou",
+    "JacquesCheminad" => "Chaminade_HS",
+    "FrançoisAsselineau" => "UPR_Asselineau",
+    "FrançoisFillon" => "FrancoisFillon"
+);
+
+
+
 // Création des fonction de récupération des valeurs par account et par hashtag
 
 
--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 
 echo "Affichage des valeurs";
 
+// Affichage du nombre de tweet par candidat depuis un mois
+// Affichage du nombre de tweet par candidat avec le hashtag
 
 ?>
