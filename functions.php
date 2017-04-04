d0824193fc6b723af7ef72b0143d81ae00944120
Benjamin Hil <benjam.hil@gmail.com>
2017-04-04 15:49:17 +0200

    adding lib, implement twitter search logic

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+require "vendor/autoload.php";
+require_once 'config.php';
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
 
 // array(NOM_CANDIDAT,TWITTER,MOTS_CLEFS)
 array(
@@ -19,5 +24,25 @@ array(
 
 // Création des fonction de récupération des valeurs par account et par hashtag
 
+define('CONSUMER_KEY', TWITTER_KEY);
+define('CONSUMER_SECRET', TWITTER_SECRET);
+define('ACCESS_TOKEN', 'your_access_token');
+define('ACCESS_TOKEN_SECRET', 'your_access_token_secret');
+
+function search(array $query)
+{
+    $toa = new TwitterOAuth(TWITTER_KEY, TWITTER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_TOKEN_SECRET);
+    return $toa->get('search/tweets', $query);
+}
+
+$query = array(
+    "q" => "",
+);
+
+$results = search($query);
+
+foreach ($results->statuses as $result) {
+    echo $result->user->screen_name . ": " . $result->text . "\n";
+}
 
 ?>
