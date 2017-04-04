6aef85f8ba08ca2afb01d5ed1a1088f1eba33f16
Thibault <th.jullion@gmail.com>
2017-04-04 15:50:03 +0200

    1

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+<div class="container-candidats">
+
+</div>
 <?php
 
 echo "Affichage des valeurs";
@@ -10,3 +22,5 @@ echo "Affichage des valeurs";
 //BONUS : Tweet de TRUMP
 
 ?>
+</body>
+</html>
